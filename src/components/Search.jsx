1aa4df11c2d5cb6d311eea4a622677c1ff3c46c6
unknown <ashkaell24@gmail.com>
2019-12-14 13:25:02 +0200

    just commit

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { AlertContext } from '../context/alert/alertContext'
 
 export const Search = () => {
+    const [value, setValue] = useState('')
+    const { show } = useContext(AlertContext)
+
+    const onSubmit = (event) => {
+        if (event.key !== 'Enter') {
+            return
+        }
+
+        if (value.trim()) {
+            console.log('Make request with: ', value);
+        } else {
+            show('Enter User Data!')
+        }
+    }
+
     return (
         <div className="form-group">
-            <input type="text" 
-            className="form-control" 
-            placeholder="Enter the name..." />
+            <input 
+                type="text"
+                className="form-control"
+                placeholder="Enter the name..."
+                value={value}
+                onChange={event => setValue(event.target.value)}
+                onKeyPress={onSubmit}
+            />
         </div>
     )
 }
