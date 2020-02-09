8b78360e5e7ab007aba5ef5c8576bd52dd16209b
unknown <ashkaell24@gmail.com>
2020-02-09 16:21:46 +0200

    redacted readme

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,22 +4,22 @@ import { Card } from '../components/Card'
 import { GitHubContext } from '../context/gitHub/gitHubContext'
 
 export const Home = () => {
-    const { loading, users } = useContext(GitHubContext)
+  const { loading, users } = useContext(GitHubContext)
 
 
-    return (
-        <div>
-            <Search />
-            <div className="row">
-                {loading
-                    ? <p className="text-center">Loading</p>
-                    : users.map(user =>
-                        <div className="col-sm-4 mb-4" key={user.id}>
-                            <Card user={user} />
-                        </div>
-                    )
-                }
+  return (
+    <div>
+      <Search />
+      <div className="row">
+        {loading
+          ? <p className="text-center">Loading</p>
+          : users.map(user =>
+            <div className="col-sm-4 mb-4" key={user.id}>
+              <Card user={user} />
             </div>
-        </div>
-    )
+          )
+        }
+      </div>
+    </div>
+  )
 }
